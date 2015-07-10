a6a617ac4af75c4e663472953a3a212f1b55b440
Sijan <sijan.s@netforce.com>
2015-07-10 10:52:39 +0700

    init

--- netforce_bookstore/models/bookstore_customer.py
+++ netforce_bookstore/models/bookstore_customer.py
@@ -10,6 +10,7 @@ class BookstoreCustomer(Model):
         "job" : fields.Char("Job"),
         "birth_date" : fields.Date("Birth Date"),
         "num_of_book_burrowed" : fields.Integer("Books Burrowed",function="get_num_of_book_burrowed"),
+        "sijan_field": fields.Integer("Int"),
     }
 
     _defaults = {
